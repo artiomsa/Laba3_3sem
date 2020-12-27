29de8b05025e88fd67b4f2f6297c7c6a4863a480
artiomsa <39264484+artiomsa@users.noreply.github.com>
2020-12-27 16:52:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Graph.hpp"
+#include "Test.hpp"
+int main() {
+    Tests();
+    int size;
+    cout << "Enter size" << endl;
+    cin >> size;
+    Graph<int> graph = Graph<int>(size);
+    graph.NewGraphFromKeyboard(size);
+    for(int i = 0; i < graph.GetSize(); i++)
+    {
+        for(int j = 0; j < graph.GetSize(); j++)
+        {
+            if(graph.Get(i,j) == INT_MAX)
+            {
+                cout << "X" << " ";
+                continue;
+            }
+            cout << graph.Get(i,j)<< " ";
+        }
+        cout << endl;
+    }
+    cout << "Enter start dot" << endl;
+    int start;
+    cin >> start;
+    start--;
+    graph.FindShortest(start);
+
+    return 0;
+}
